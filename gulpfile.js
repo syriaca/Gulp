b393535a39164b3e04d4bba9dfe0b868b2375bb8
Jean-Yves Le Cabellec <jy.lecabellec@gmail.com>
2018-07-04 09:56:45 +0200

    Added sass compilation and sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@
 var gulp =  require('gulp'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
-    rename = require('gulp-rename');
+    rename = require('gulp-rename'),
+    sass = require('gulp-sass'),
+    maps = require('gulp-sourcemaps');
 
 
 gulp.task('concat', function(){
@@ -23,8 +25,14 @@ gulp.task('minifyScripts', function() {
     .pipe(gulp.dest('js'));
 });
 
-gulp.task('default', ['concat', 'minifyScripts'], function(){
-    console.log('this is just a default task');
+gulp.task('compileSass', function(){
+    gulp.src('scss/application.scss')
+    .pipe(maps.init())
+    .pipe(sass())
+    .pipe(maps.write('./'))
+    .pipe(gulp.dest('css'))
 });
 
-gulp.task
+gulp.task('default', ['concat', 'minifyScripts', 'compileSass'], function(){
+    console.log('this is just a default task');
+});
